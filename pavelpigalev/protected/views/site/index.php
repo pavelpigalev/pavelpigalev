d4394c8ab220700bedf1c5196b23bd658bd76b19
Pavel Pigalev <pavel.pigalev@gmail.com>
2014-07-01 01:10:49 +0400

    first development commit

--- pavelpigalev/protected/views/site/index.php
+++ pavelpigalev/protected/views/site/index.php
@@ -1,8 +1,7 @@
 <?php
 /* @var $this SiteController */
 
-$this->pageTitle = $this->_app->name;
+$this->pageTitle = $this->_app->name . '| development';
 ?>
 
 <h1>Добро пожаловать на <i><?= CHtml::encode($this->_app->name); ?></i></h1>
-
